80edaee38e607bfcaa272368a26ddddd06726555
Brian Gonzalez <me@briangonzalez.org>
2017-06-16 07:58:46 -0700

    cleanup

--- scratch/buddy-search.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/osascript -l JavaScript
-
-const app = Application('Messages')
-const service = app.services()[0]
-console.log(service.name());
-console.log();
-
-const list = service.buddies.whose({ name: { _equals: 'Anthony Balmeo' } })
-console.log(list[0]);
--- scratch/scratch-2.js
+++
@@ -1,8 +0,0 @@
-var osascript = require('node-osascript');
-
-osascript.executeFile('applescripts/buddy-search.scpt',
-  { searchQuery: 'stef' },
-  (err, result, raw) => {
-    if (err) return console.error(err)
-    console.log(result)
-  });
