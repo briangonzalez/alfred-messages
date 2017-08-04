71de9a79c07a0daad8cd0563c0941e4f0b44f4ca
Brian Gonzalez <me@briangonzalez.org>
2017-08-04 15:35:43 -0700

    Fixes #4 / Fixes #7 - sort by frequency / fit long messages into UI

--- index.js
+++ index.js
@@ -3,26 +3,33 @@ const helpers = require('./lib/applescript-helpers.js');
 
 const input = (alfy.input || 'brian this is a message').split(' ');
 const query = input[0];
-const message = input.slice(1, input.length).join(' ');
+const message = input.slice(1, input.length).join(' ').trim();
 
 (async () => {
-	let output = await helpers.buddySearch(query);
-	if (!output) {
-		return alfy.output([{
-			title: `No results found for ${query}`,
-			subtitle: 'Please try another query'
-		}]);
-	}
+  let output = await helpers.buddySearch(query);
+  if (!output) {
+    return alfy.output([{
+      title: `No results found for ${query}`,
+      subtitle: 'Please try another query'
+    }]);
+  }
 
-	alfy.output(output.map(buddy => {
-		return {
-			title: buddy.name,
-			subtitle: message ? `Send "${message}" to ${buddy.handle}` : `Type to send message to ${buddy.handle}`,
-			arg: JSON.stringify({
-				handle: buddy.handle,
-				name: buddy.name,
-				message
-			})
-		}
-	}));
+  alfy.output(output.map(buddy => {
+    let displayMessage = message
+    if (message.length > 60) {
+      const first = message.slice(0, 20)
+      const last = message.slice(message.length - 20, message.length)
+      displayMessage = `${first}.............${last}`
+    }
+
+    return {
+      title: buddy.name,
+      subtitle: message ? `Send "${displayMessage}" to ${buddy.handle}` : `Type to send message to ${buddy.handle}`,
+      arg: JSON.stringify({
+        handle: buddy.handle,
+        name: buddy.name,
+        message
+      })
+    }
+  }));
 })()
